e46eed49986868b86ae112aca20b63bd6769f4f0
imxande <alerodriguezlarosa@gmail.com>
2020-07-12 12:39:20 -0400

    added randomize implementation

--- index.js
+++ index.js
@@ -401,8 +401,14 @@ function getHTML(/* Code here */) {
 
 /* STRETCH 2: Create a function called `randomize` that takes a data array as an argument and returns a the same array in a randomized order. */
 
-function randomize(/* Code here */) {
+function randomize(array) {
   /* Code here */
+  const result = array.sort(function (a, b) {
+    return 0.5 - Math.random();
+  });
+  return result;
 }
+
+randomize(artists);
 
 /* STRETCH 3: Use advanced array methods (.map, .reduce, .filer) to refactor your MVP code (create an array of all artists born in the 1900s with .filter, for example) */
